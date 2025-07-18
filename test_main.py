cc05a2b00aaf3cde4ac986275ddd09dca5344eaf
Paul Lennon <paul.lennon@atu.ie>
2025-07-18 11:09:43 +0100

    Added update test

--- test_main.py
+++ test_main.py
@@ -11,10 +11,30 @@ def test_get_users_initially_empty():
 def test_post_user():
     user = {
         "user_id": 1,
-        "name": "Mary",
-        "email": "mary@example.com",
+        "name": "Paul L",
+        "email": "Paul@atu.ie",
         "age": 22
     }
     response = client.post("/api/users", json=user)
     assert response.status_code == 201
-    assert response.json()["name"] == "Mary"
+    assert response.json()["name"] == "Paul L"
+
+def test_put_user():
+
+    user = {
+        "user_id": 1,
+        "name": "Mary",
+        "email": "mary@atu.ie",
+        "age": 22
+    }
+    response = client.post("/api/users", json=user)
+
+    user = {
+        "user_id": 1,
+        "name": "Tom",
+        "email": "tom@atu.ie",
+        "age": 52
+    }
+    response = client.put("/api/users/1", json=user)
+    assert response.status_code == 200
+    assert response.json()["name"] == "Tom"
